a44b30f3375268a7d176c9871d6d2438567e94b4
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2018-04-08 12:08:06 +0200

    Updated build script & increased kernel log buffer size
    Change-Id: Ic59d30324d359b6084cc617a0a8d1b61ee400ef8

--- 0and.sh
+++ 0and.sh
@@ -1,6 +1,8 @@
 # this script has to be ran in the root of lineage repository
 source build/envsetup.sh
 
+# run 'brunch mido' before making the bootimage
+alias btmg="make bootimage -j4 TARGET_KERNEL_CONFIG=sensei_defconfig"
 alias kl="cd kernel/xiaomi/msm8953; source 1krnl.sh"
 export USE_CCACHE=1
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
